e331d2da91003d8d631ba3a14873dd4bb510d3bb
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-22 23:07:38 +0100

    Add the ability to map the external GITHUB_WORKFLOW path to an internal one that points to /github/workspace to make lcov happy with the paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -eu
 
-cd $GITHUB_WORKSPACE
+REPOSITORY_NAME=$(basename -- $RUNNER_WORKSPACE)
 
-export
+# Map the directory used for the build to the github workspace to make lcov happy
+mv -p $RUNNER_WORKSPACE
+ln -s $GITHUB_WORKSPACE $RUNNER_WORKSPACE/$REPOSITORY_NAME
+
+cd $GITHUB_WORKSPACE
 
 /usr/bin/lcov --gcov-tool $INPUT_GCOV_PATH $INPUT_PARAMS
