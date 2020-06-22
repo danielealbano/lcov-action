83dd170d81f2eafce53fd5cd2ad00fb3c8a4cf80
Daniele Salvatore Albano <d.albano@gmail.com>
2020-06-23 00:39:17 +0100

    Make the action more smart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
+TEMP_LCOV_PATH=~/.lcov/
 REPOSITORY_NAME=$(basename -- $RUNNER_WORKSPACE)
 
 # Map the directory used for the build to the github workspace to make lcov happy
@@ -9,6 +10,25 @@ ln -s $GITHUB_WORKSPACE $RUNNER_WORKSPACE/$REPOSITORY_NAME
 
 cd $GITHUB_WORKSPACE
 
-echo "/usr/bin/lcov --gcov-tool ${INPUT_GCOV_PATH} ${INPUT_PARAMS}"
+mkdir -p $TEMP_LCOV_PATH
+[[ -f temp-lcov.info ]] rm temp-lcov.info
 
-/usr/bin/lcov --gcov-tool $INPUT_GCOV_PATH $INPUT_PARAMS
+# Generate the initial lcov version and strips out anything related to /usr
+/usr/bin/lcov --gcov-tool $INPUT_GCOV_PATH -i -c -d . -o $TEMP_LCOV_PATH/info
+/usr/bin/lcov --gcov-tool $INPUT_GCOV_PATH -r $TEMP_LCOV_PATH/info '/usr/*' -o $TEMP_LCOV_PATH/filtered
+
+if [ -n "$INPUT_REMOVE_PATTERNS" ];
+then
+    # Strips out the requested paths
+    IFS=, read -ra REMOVE_PATTERNS_ARR <<< "$INPUT_REMOVE_PATTERNS"
+    for REMOVE_PATTERN in "${REMOVE_PATTERNS_ARR[@]}"
+    do
+        /usr/bin/lcov --gcov-tool $INPUT_GCOV_PATH -r $TEMP_LCOV_PATH/filtered "*${REMOVE_PATTERN}*" -o $TEMP_LCOV_PATH/filtered
+    done
+fi
+
+# Logging
+/usr/bin/lcov --gcov-tool $INPUT_GCOV_PATH -l $TEMP_LCOV_PATH/filtered
+
+# Copy the filtered lcov output to the requested path
+cp $TEMP_LCOV_PATH/filtered $INPUT_OUTPUT_LCOV_INFO
